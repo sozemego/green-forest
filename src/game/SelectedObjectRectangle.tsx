7727c1a378486cbbe88f555ad7c77ea135179bec
sozemego <piknypiec@gmail.com>
2020-02-26 20:03:19 +0100

    SelectedObjectRectangle.tsx - added animation to the rectangle

--- src/game/SelectedObjectRectangle.tsx
+++ src/game/SelectedObjectRectangle.tsx
@@ -1,7 +1,15 @@
 import { useGameService } from "./useGameService";
-import React, { useMemo } from "react";
-import { Geometry, Shape } from "three";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import {
+  AnimationClip,
+  AnimationMixer,
+  Geometry,
+  Shape,
+  VectorKeyframeTrack
+} from "three";
 import { HasPosition, HasWidth } from "./util";
+import { useFrame } from "react-three-fiber";
+import { DELTA } from "./Constants";
 
 export function SelectedObjectRectangle() {
   let gameService = useGameService();
@@ -23,13 +31,65 @@ function Rectangle({ selectedObject }: RectangleProps) {
     return shape;
   }, [width, height]);
 
+  let line = useRef();
+
   let geometry = useMemo(
     () => new Geometry().setFromPoints(shape.getPoints()),
     [shape]
   );
 
+  let animationClip = useMemo(() => {
+    let scaleKeyFrame = new VectorKeyframeTrack(
+      ".scale",
+      [0, 0.1, 0.25, 0.5, 0.6, 0.75, 0.9, 1],
+      [
+        1,
+        1,
+        1,
+        1.05,
+        1.05,
+        1,
+        1.1,
+        1.1,
+        1,
+        1.15,
+        1.15,
+        1,
+        1.2,
+        1.2,
+        1,
+        1.15,
+        1.15,
+        1,
+        1.05,
+        1.05,
+        1,
+        1,
+        1,
+        1
+      ]
+    );
+    return new AnimationClip("work", 1, [scaleKeyFrame]);
+  }, []);
+
+  let [mixer, setMixer] = useState();
+
+  useEffect(() => {
+    let mixer = new AnimationMixer(line.current!);
+    let clipAction = mixer.clipAction(animationClip);
+    clipAction.play();
+    setMixer(mixer);
+  }, []);
+
+  useFrame(() => {
+    if (!mixer) {
+      return;
+    }
+    mixer.update(DELTA);
+  });
+
   return (
-    <lineLoop position={[x, y, 0.1]}>
+    <lineLoop position={[x, y, 0.1]} ref={line}>
       <primitive object={geometry} attach={"geometry"} />
       <lineBasicMaterial attach={"material"} linewidth={1.25} color={"gold"} />
     </lineLoop>
